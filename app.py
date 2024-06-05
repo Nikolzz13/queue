71a9d555a3ad5c5409ade1bcf304288eb9eb94ab
Nikita <122480964+Nikolzz13@users.noreply.github.com>
2024-06-05 14:25:48 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template
 
 app = Flask (__name__)
 
-@app.route ('/index')
-def user(username):
+@app.route ('/')
+def index():
     listmap = [{'Name':"Проф. орентация","link":123},{'Name':"Забрать документы","link":123},{'Name':"Забрать документы","link":123},{'Name':"Донести Документ","link":123}]
     return render_template("index.html",listed = listmap)
 @app.route('/monitor')
 def monitor():
     return render_template("monitor.html")
 @app.route('/meneger')
-def index():
+def meneger():
     return render_template("meneger.html")
 @app.route('/operator')
-def nado():
+def operator():
     return render_template("operator.html")
 @app.route('/admin')
 def admin():
